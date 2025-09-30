2fb79abba311adf55fff5a9fafd35f2629413f6a
LeJ <Le.j9@outlook.fr>
2025-09-30 18:54:11 +0200

    Add Graphik font family and update styles
    - Introduced various styles of the Graphik font family including Black, Bold, Medium, Light, and their italic counterparts.
    - Updated the main stylesheet to import the new fonts and applied them throughout the site.
    - Enhanced the layout and design of the homepage, including sections for finding artisans and featured artisans of the month.
    - Created new routes for legal pages (mentions légales, données personnelles, accessibilité, cookies) and corresponding views.
    - Improved the layout structure in the main layout file for better organization and responsiveness.
    - Added new images for the logo and favicons.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Trouve ton artisan !" });
+});
+
+/* Pages légales */
+router.get("/mentions-legales", function (req, res, next) {
+  res.render("mentions-legales", {
+    title: "Mentions légales - Trouve ton artisan !",
+  });
+});
+
+router.get("/donnees-personnelles", function (req, res, next) {
+  res.render("donnees-personnelles", {
+    title: "Données personnelles - Trouve ton artisan !",
+  });
+});
+
+router.get("/accessibilite", function (req, res, next) {
+  res.render("accessibilite", {
+    title: "Accessibilité - Trouve ton artisan !",
+  });
+});
+
+router.get("/cookies", function (req, res, next) {
+  res.render("cookies", { title: "Cookies - Trouve ton artisan !" });
 });
 
 module.exports = router;
