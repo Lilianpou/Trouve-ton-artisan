8ebda8b4dcbfde6db72d0d9c108ea561f4f327a8
LeJ <Le.j9@outlook.fr>
2025-10-02 23:06:27 +0200

    Modify CORS to support multiple frontend URLs
    Updated CORS configuration to allow multiple origins.

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var app = express();
 // Configuration CORS
 app.use(
   cors({
-    origin: "http://localhost:3000", // URL du frontend React
+    origin: 
+    "http://localhost:3000",
+    'https://trouve-ton-artisan-chi.vercel.app/'// URL du frontend React
     credentials: true,
   })
 );
